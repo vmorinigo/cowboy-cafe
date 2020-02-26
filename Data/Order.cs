4559c08f8e0509a7f66dd357ca55474920841923
vmorinigo <vmorinigo@F1092-40-2313>
2020-02-26 15:09:25 -0600

    pos2.3

--- Data/Order.cs
+++ Data/Order.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
 
 namespace CowboyCafe.Data
 {
-    public class Order
+    public class Order : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private uint lastOrderNumber;
 
-        private List<IOrderItem> items;
+        private List<IOrderItem> items = new List<IOrderItem>();
 
-        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+        public IEnumerable<IOrderItem> Items => items.ToArray(); //makes a copy
 
         public double Subtotal => 0;
 
@@ -18,12 +21,14 @@ namespace CowboyCafe.Data
 
         public void Add(IOrderItem item)
         {
-
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
         }
 
         public void Remove(IOrderItem item)
         {
-
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
         }
     }
 }
