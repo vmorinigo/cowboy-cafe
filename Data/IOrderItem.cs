f665f16ea6dc17086c3ca4b3ea3a5f54a85dd5f9
vmorinigo <vmorinigo@F1092-40-2313>
2020-02-26 14:55:34 -0600

    POS2.1

---
+++ Data/IOrderItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    /// <summary>
+    /// An interface represeting a single item of an order 
+    /// </summary>
+    public interface IOrderItem
+    {
+        /// <summary>
+        /// The price od this order item
+        /// </summary>
+        double Price { get; }
+        /// <summary>
+        /// Special Instructions for this order item
+        /// </summary>
+        List<string> SpecialInstructions { get; }
+    }
+}
---
+++ Data/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public class Order
+    {
+        private uint lastOrderNumber;
+
+        private List<IOrderItem> items;
+
+        public IEnumerable<IOrderItem> Items => throw new NotImplementedException();
+
+        public double Subtotal => 0;
+
+        public uint OrderNumber { get; }
+
+        public void Add(IOrderItem item)
+        {
+
+        }
+
+        public void Remove(IOrderItem item)
+        {
+
+        }
+    }
+}
