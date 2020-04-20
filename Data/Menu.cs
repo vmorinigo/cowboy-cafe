b4f521ad401d761c245c62f30df820ad70e5f845
vmorinigo <vmorinigo@CIS-RDS2>
2020-04-20 18:19:16 -0500

    web3

---
+++ Data/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public static class Menu
+    {
+        public static List<IOrderItem> items = new List<IOrderItem>();
+
+        public static IEnumerable<IOrderItem> Entrees
+        {
+            get
+            {
+                foreach(IOrderItem i in items)
+                {
+                    if(i is Entree)
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+
+        public static IEnumerable<IOrderItem> CompleteMenu { get { return items; } }
+
+    }
+}
