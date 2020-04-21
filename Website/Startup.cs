a58b8fb51beef7c30cafedca7fc8257b16a35f33
vmorinigo <vmorinigo@CIS-RDS2>
2020-04-20 20:32:43 -0500

    weweb

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -29,7 +29,7 @@ namespace Website
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (false)
             {
                 app.UseDeveloperExceptionPage();
             }
